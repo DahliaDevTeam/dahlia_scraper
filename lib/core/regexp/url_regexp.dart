4a95acae1c93b28435a1c9e6c08ed969ead07593
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-03-02 20:32:29 +0100

    [core] url regexp validation more flexible

--- lib/core/regexp/url_regexp.dart
+++ lib/core/regexp/url_regexp.dart
@@ -1,14 +1,15 @@
 final class UrlRegexp {
 
-  static final RegExp validation = RegExp(r"^https?://(www\.)?[-a-zA-Z\d@:%._+~#=]{1,256}\.[a-zA-Z\d()]{1,6}\b([-a-zA-Z\d()@:%_+.~#?&/=]*)$");
-  static final RegExp assetValidation = RegExp(r'^https?://(www\.)?[-a-zA-Z\d@:%._+~#=]{1,256}\.[a-zA-Z\d()]{1,6}\b([-a-zA-Z\d()@:%_+.~#?&/=]*).(?:jpg|gif|png)$');
+  static final RegExp validation = RegExp(r"^https?://([-a-z-A-Z]+\.)?[-a-zA-Z\d@:%._+~#=]{1,256}\.[a-zA-Z\d()]{1,6}\b([-a-zA-Z\d()@:%_+,.~#?&/=]*)$");
+  static final RegExp assetValidation = RegExp(r'^https?://([-a-z-A-Z]+\.)?[-a-zA-Z\d@:%._+~#=]{1,256}\.[a-zA-Z\d()]{1,6}\b([-a-zA-Z\d()@:%_+,.~#?&/=]*).(?:jpg|gif|png)$');
 }
 
 extension UrlValidation on String? {
 
   bool get isValidUrl {
     if (this == null) return false;
-    return UrlRegexp.validation.hasMatch(this!);
+    return UrlRegexp.validation.hasMatch(this!)
+      || UrlRegexp.assetValidation.hasMatch(this!);
   }
 
 }
